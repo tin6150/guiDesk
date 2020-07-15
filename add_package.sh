8ad810e12a4031bde437837cc40aa4d813fd0042
tin6150 <tin+zink@grumpyxmas.com>
2020-07-14 18:02:11 -0700

    add a few more xfce4 packages

--- add_package.sh
+++ add_package.sh
@@ -4,7 +4,10 @@ yum -ty install epel-release
 yum -ty groupinstall "Xfce"
 yum -ty install lightdm xorg-x11-drv-libinput.x86_64
 yum -ty install firefox
-yum -ty install xfe
+yum -ty install xfce4-about xfce4-eyes-plugin  xfce4-screenshooter xfce4-screenshooter-plugin 
+yum -ty install ristretto  # image viewer
+yum -ty install thunar # should be installed already.  this is a gui file manager
+#yum -ty install xfe twm file manager, not avail anymore
 #systemctl set-default multi-user.target
 systemctl disable gdm
 systemctl enable lightdm
